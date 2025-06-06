fb1f7cabb918b0209b69efc0f07880c2aaa82d84
Arthur <arn203@exeter.ac.uk>
2025-06-06 15:41:52 +0100

    basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using CooccurrenceRegression
+using CooccurrenceRegression, Random
 using Test
-
+X = 1 .- rand(LKJ(50,1))
+Y = rand(Bool,3,50)
 @testset "CooccurrenceRegression.jl" begin
-    # Write your tests here.
+    Random.seed!(123)
+    chn1 =cooccurrence_regression(X,Y)
+    Random.seed!(123)
+    chn2 =cooccurrence_regression([X],Y)
+    @test chn1[:α] == chn2[:α]
 end
